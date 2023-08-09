cffe4fd8f856847c299019dd74e2a79f67564477
82103 <82103@LAPTOP-J98VK73J>
2023-08-09 16:09:37 +0900

    score2

---
+++ javaEx03/src/ch12/unit02/score2/App.java
@@ -0,0 +1,11 @@
+package ch12.unit02.score2;
+
+public class App {
+
+	public static void main(String[] args) {
+		ScoreUI ui = new ScoreUI();
+		ui.menu();
+
+	}
+
+}
---
+++ javaEx03/src/ch12/unit02/score2/Score.java
@@ -0,0 +1,16 @@
+package ch12.unit02.score2;
+
+import java.util.List;
+
+public interface Score {
+	public void insertScore(ScoreVO vo); // 데이터등록
+
+	public List<ScoreVO> listScore(); // 전체 리스트
+
+	public ScoreVO readScore(String hak); // 학번 검색
+
+	public boolean deleteScore(String hak); // 삭제
+
+	public List<ScoreVO> listScore(String name); // 이름 검색
+
+}
